b24dbdd1cf4e1b8705443977a8f4b557d33ca6ed
Kara Bayse <karabayse@gmail.com>
2017-12-05 12:27:28 -0600

    injected controllers into home and answers views

--- public/scripts/scoreController.js
+++ public/scripts/scoreController.js
@@ -12,11 +12,44 @@ myApp.controller('ScoreController', function(ScoreService, $http, $location) {
     vm.options = {
     legend: {
       display: true
-      } // end legend
-    }; // end vm.options
-    vm.colors = ["#FF82AB",
+    }, // end legend
+    colors: ["#FF82AB",
             "rgba(224, 108, 112, 1)",
-            "rgba(224, 108, 112, 1)"];     
+            "rgba(224, 108, 112, 1)"]
+    }; // end vm.options
   };  // end getPieChart
+
+  // get score function
+  vm.getScore = function() {
+
+
+    // Use getElementById ?
+
+    // If first input is "ou", Correct!
+    //   else Incorrect
+    // If second input is "oy", Correct!
+    //   else Incorrect
+    // If third input is "ow", Correct!
+    //   else Incorrect
+    // If fourth input is "oi", Correct!
+    //   else Incorrect
+    // If fifth input is "ou", Correct!
+    //   else Incorrect
+    // If sixth input is "ou", Correct!
+    //   else Incorrect
+    // If seventh input is "ow", Correct!
+    //   else Incorrect
+    // If eighth input is "ow", Correct!
+    //   else Incorrect
+    // If ninth input is "ai", Correct!
+    //   else Incorrect
+
+    // Pie Chart Tags:  Correct, Incorrect, Not Answered
+    // Change to Correct or Incorrect ?
+    // If not, need to change conditional statements above
+
+    // Push into array or Correct, Incorrect, Not Answered -> labels array ?
+
+  }; // end getScore function
 
 }); // end controller
